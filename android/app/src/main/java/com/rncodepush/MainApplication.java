557e9ebd748d9ae297cf82ab645ed0c30146f57d
Loreto Frallon <loretofrallon@gmail.com>
2024-01-08 17:00:55 +0800

    chore: :wrench: config app to use code push

--- android/app/src/main/java/com/rncodepush/MainApplication.java
+++ android/app/src/main/java/com/rncodepush/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+import com.microsoft.codepush.react.CodePush;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -41,6 +43,14 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected Boolean isHermesEnabled() {
           return BuildConfig.IS_HERMES_ENABLED;
+        }
+
+        // Override the getJSBundleFile method in order to let
+        // the CodePush runtime determine where to get the JS
+        // bundle location from on each app start
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
       };
 

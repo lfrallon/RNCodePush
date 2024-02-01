3f8a88cc9ed8cd411d7d4817b86b9beb4b5cce82
Loreto Frallon <loretofrallon@gmail.com>
2024-02-01 11:43:42 +0800

    chore: :wrench: updated es-lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
+/* eslint-env node */
 module.exports = {
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'plugin:@typescript-eslint/eslint-recommended',
+    // 'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
   root: true,
-  extends: '@react-native-community',
+  overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      files: ['./**/*.{ts,tsx}', '__tests__/**/*'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
